3e634640fb0cea17fea67e0dd4560d3b61c68f20
guylamar2006 <guychronister@outlook.com>
2024-02-10 15:30:02 -0600

    Added F7A0121

--- steamdeck-BIOS-manager.sh
+++ steamdeck-BIOS-manager.sh
@@ -137,6 +137,9 @@ then
 	echo downloading BIOS 120
 	curl -s -O --output-dir $(pwd)/BIOS/ -L https://gitlab.com/evlaV/jupiter-hw-support/-/raw/a43e38819ba20f363bdb5bedcf3f15b75bf79323/usr/share/jupiter_bios/F7A0120_sign.fd
 
+	echo downloading BIOS 121
+	curl -s -O --output-dir $(pwd)/BIOS/ -L https://gitlab.com/evlaV/jupiter-hw-support/-/raw/7ffc22a4dc083c005e26676d276bdbd90dd1de5e/usr/share/jupiter_bios/F7A0121_sign.fd
+
 	echo BIOS download complete!
 
 elif [ "$Choice" == "FLASH" ]
